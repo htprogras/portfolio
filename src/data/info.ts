c18d979667953176d471c5a1a7c6df626bbd5633
htprogras <htprogras@gmail.com>
2023-02-08 16:59:05 +0100

    first commit

---
+++ src/data/info.ts
@@ -0,0 +1,37 @@
+export const info = {
+  baseUrl: "https://taissir.me",
+  name: "Taissir Tigzirt",
+  jobDescription: "Web Developer",
+  about: `Hello and welcome to my portfolio!
+  I am a web developer with a strong focus on building front-end applications using modern JavaScript frameworks such as React JS, Next JS, and Gatsby JS. 
+  With several years of experience in web development, I have a proven track record of delivering high-quality and scalable web solutions. 
+  As a detail-oriented and professional developer, I am committed to delivering high-quality work and meeting agreed upon deadlines. 
+  Please don't hesitate to contact me if you have any questions or if you would like to discuss your project in detail.
+  `,
+
+  socialMedia: {
+    github: "https://github.com/htprogras",
+    email: "mailto:htprogras@gmail.com",
+  },
+
+  projects: [
+    {
+      title: "Kemené Technology",
+      isFeatured: true,
+      thumbnail: "/assets/images/kemene.png",
+      liveUrl: "https://kemene.com/",
+    },
+    {
+      title: "Dart Scope",
+      isFeatured: true,
+      thumbnail: "/assets/images/dart scope.png",
+      liveUrl: "https://dartscope.vercel.app/",
+    },
+    {
+      title: "Part Finder",
+      isFeatured: true,
+      thumbnail: "/assets/images/part finder.png",
+      liveUrl: "https://partfinder.app/",
+    }
+  ],
+};
---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+export interface IMetaHead {
+  title: string;
+  description: string;
+  ogImageUrl: string;
+}
+
+export interface IHeroProps {
+  name: string;
+  about: string;
+}
+
+export interface IProject {
+  title: string;
+  isFeatured: boolean;
+  thumbnail: string;
+  githubUrl: string;
+  liveUrl: string;
+}
+export interface IProjects {
+  projects: IProject[];
+}
+
+export interface IProjectDetails {
+  projectDetail: IProject;
+}
